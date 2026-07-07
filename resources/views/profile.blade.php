12b8feea6890f34028fec962800533f02d96f4a8
NYSAJ <pugpug11@gmail.com>
2026-07-08 00:28:23 +0900

    refactor: use shared Blade layout

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Profile</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('title', 'プロフィールページ')
+
+@section('content')
     <h1>Profile</h1>
 
     <h2>ようこそ{{ $name }}さん！</h2>
@@ -32,5 +28,4 @@
         <button>ログイン</button>  
     @endguest
 
-</body>
-</html>
+@endsection
